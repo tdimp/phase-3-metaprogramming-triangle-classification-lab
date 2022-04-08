0eaa886fb8be2051f022b264c74b1b14dd0a4722
Thomas Dimperio <twdimperio@gmail.com>
2022-04-08 08:44:41 -0600

    commit tree 97beab5ed51cfae7746018d2615e68a4528068ff

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c 
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_positive?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def inequality?
+    a + b > c && b + c > a && a + c > b
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_positive? && inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
